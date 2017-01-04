1a9ff4901fe15370f694ed38acaf7c4fd8028239
eongoo <eongoo@qq.com>
2017-01-04 15:14:25 +0800

    16 - Working with Observables - Using flatMap to Process Inner Observables

--- main.ts
+++ main.ts
@@ -3,26 +3,44 @@ import {Observable} from "rxjs";
 let output = document.getElementById("output");
 let button = document.getElementById("button");
 
-let source = Observable.fromEvent(button, "click");
+let click = Observable.fromEvent(button, "click");
 
 function load(url: string) {
-  let xhr = new XMLHttpRequest();
+  return Observable.create(observer => {
+    let xhr = new XMLHttpRequest();
 
-  xhr.addEventListener("load", () => {
-    let movies = JSON.parse(xhr.responseText);
+    xhr.addEventListener("load", () => {
+      let data = JSON.parse(xhr.responseText);
+      observer.next(data);
+      observer.complete();
+    });
+
+    xhr.open('GET', url);
+    xhr.send();
+  });
+}
+
+function renderMovies(movies) {
     movies.forEach(m => {
       let div = document.createElement('div');
       div.innerText = m.title;
       output.appendChild(div);
     });
-  });
-
-  xhr.open('GET', url);
-  xhr.send();
 }
 
-source.subscribe(
+load("movies.json").subscribe(renderMovies);
+
+click.flatMap(e => load("movies.json"))
+     .subscribe(
+        renderMovies,
+        e => console.log(`error: ${e}`),
+        () => console.log("complete")
+     );
+
+/*
+click.subscribe(
   e => load("movies.json"),
   e => console.log(`error: ${e}`),
   () => console.log("complete")
 );
+*/
