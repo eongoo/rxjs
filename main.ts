6911fa7184eab0ffcb1b33372509bfa8850beca4
eongoo <eongoo@qq.com>
2017-01-04 17:11:48 +0800

    17 - Working with Observables - Implementing Retry Logic with retry and retryWhen

--- main.ts
+++ main.ts
@@ -10,16 +10,31 @@ function load(url: string) {
     let xhr = new XMLHttpRequest();
 
     xhr.addEventListener("load", () => {
-      let data = JSON.parse(xhr.responseText);
-      observer.next(data);
-      observer.complete();
+      if (xhr.status === 200) {
+        let data = JSON.parse(xhr.responseText);
+        observer.next(data);
+        observer.complete();
+      } else {
+        observer.error(xhr.statusText);
+      }
     });
 
     xhr.open('GET', url);
     xhr.send();
-  });
+  }).retryWhen(retryStrategy({attemps: 3, delay: 1500}));
 }
 
+function retryStrategy({attemps = 4, delay = 1000}) {
+  return function(errors) {
+    return errors
+              .scan((acc, value) => {
+                console.log(acc, value); 
+                return acc + 1;
+              }, 0)
+              .takeWhile(acc => acc < attemps)
+              .delay(delay);
+  }
+}
 function renderMovies(movies) {
     movies.forEach(m => {
       let div = document.createElement('div');
