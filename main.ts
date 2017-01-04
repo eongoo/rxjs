4829c736e9e3d1ec92ddaa65b66e50dd3a3574b8
eongoo <eongoo@qq.com>
2017-01-04 14:11:33 +0800

    11 - Observers and Observables - Importing Just What We Need

--- main.ts
+++ main.ts
@@ -1,4 +1,7 @@
 import {Observable} from "rxjs";
+//import {Observable} from "rxjs/Observable";
+//import "rxjs/add/operator/map"
+//import "rxjs/add/operator/filter"
 
 let numbers = [1, 5, 10];
 let source = Observable.create(observer => {
@@ -23,25 +26,3 @@ source.subscribe(
   e => console.log(`error: ${e}`),
   () => console.log("complete")
 );
-
-//import {Observable, Observer} from "rxjs";
-/*
-let source = Observable.from(numbers);
-*/
-//  observer.complete();
-/*
-class MyObserver implements Observer<number>{
-  next(value) {
-    console.log(`value: ${value}`);
-  }
-
-  error(e) {
-    console.log(`error: ${e}`);
-  }
-
-  complete() {
-    console.log("complete");
-  }
-}
-source.subscribe(new MyObserver());
-*/
