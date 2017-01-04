040701d9b3d8e735127f0ba7c254f2b1f5807de0
eongoo <eongoo@qq.com>
2017-01-04 14:01:05 +0800

    10 - Observers and Observables - Using RxJS Operators

--- main.ts
+++ main.ts
@@ -1,1 +1,47 @@
-alert('ok!!');
+import {Observable} from "rxjs";
+
+let numbers = [1, 5, 10];
+let source = Observable.create(observer => {
+  let index = 0;
+  let produceValue = () => {
+    observer.next(numbers[index++]);
+
+    if(index < numbers.length) {
+      setTimeout(produceValue, 2000);
+    } else {
+      observer.complete();
+    }
+  }
+
+  produceValue();
+
+}).map(n => n * 2)
+  .filter(n => n > 4);
+
+source.subscribe(
+  value => console.log(`value: ${value}`),
+  e => console.log(`error: ${e}`),
+  () => console.log("complete")
+);
+
+//import {Observable, Observer} from "rxjs";
+/*
+let source = Observable.from(numbers);
+*/
+//  observer.complete();
+/*
+class MyObserver implements Observer<number>{
+  next(value) {
+    console.log(`value: ${value}`);
+  }
+
+  error(e) {
+    console.log(`error: ${e}`);
+  }
+
+  complete() {
+    console.log("complete");
+  }
+}
+source.subscribe(new MyObserver());
+*/
